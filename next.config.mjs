127aa0ea1cff3929bb9e32f22ed077af4d178aab
Igor Trentini <igortrentini.2004@gmail.com>
2025-12-08 17:15:21 -0300

    test: Image build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: '/logae-christmas-slider-2025',
+  assetPrefix: '/logae-christmas-slider-2025',
 }
 
 export default nextConfig
